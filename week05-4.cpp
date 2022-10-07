728d169052bebe1cd2476efea20209b36eec8793
mark891014 <114201407+mark891014@users.noreply.github.com>
2022-10-07 11:50:53 +0800

    Add files via upload

---
+++ week05-4.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    for(i=0;i<5;i++){
+        printf("電腦從0開始 , i:%d\n",i);
+    }
+        printf("\n");
+    for(i=1;i<=5;i++){
+        printf("人類從1開始 , i:%d\n",i);
+    }
+        printf("\n");
+    for(i=5;i>0;i--){
+        printf("倒過來的迴圈 ,i:%d\n",i);
+    }
+}
