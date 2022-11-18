8754930698b713ede62f2d50ddde9f7369dbebd8
mark891014 <mark950805@gmail.com>
2022-11-18 11:37:16 +0800

    修改目錄

---
+++ week11/week11-3.cpp
@@ -0,0 +1,19 @@
+///口訣: 從左到右巡一輪，兩兩比較，不對就交換
+#include <stdio.h>
+int a[10]={90,80,70,60,50,40,30,20,10,0};
+
+int main()
+{
+    for(int i=0;i<10;i++) printf("%d ",a[i]);
+    printf("\n");
+
+    for(int i=0;i<10;i++){
+        if(a[i]>a[i+1]){///希望左小又大，但不對...
+            int temp=a[i];///不對就交換
+            a[i]=a[i+1];
+            a[i+1]=temp;
+        }
+    }
+    for(int i=0;i<10;i++) printf("%d ",a[i]);
+    printf("\n");
+}
---
+++ week11/week11-5.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int a[10]={90,80,70,60,50,40,30,20,10,0};
+
+int main()
+{
+    for(int i=0;i<10;i++) printf("%d ",a[i]);
+    printf("\n");
+
+    for(int k=0;k<10;k++){
+        for(int i=0;i<10-1;i++){///巡一輪
+            if(a[i]>a[i+1]){///希望左小又大
+                int temp=a[i];///不對就交換
+                a[i]=a[i+1];
+                a[i+1]=temp;
+            }
+        }
+        for(int i=0;i<10;i++) printf("%d ",a[i]);
+        printf("\n");
+    }
+}
